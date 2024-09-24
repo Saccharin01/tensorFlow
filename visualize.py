f23f1f0310c9af714e15683d37bb34567fb26e2d
Saccharin01 <chousik01@gmail.com>
2024-09-25 01:37:04 +0900

    fix | visualize.py

--- visualize.py
+++ visualize.py
@@ -1,13 +1,13 @@
 import matplotlib.pyplot as plt
 
-def plot_history(history):
+def plot_history(history, title=None):
     plt.figure(figsize=(12, 4))
 
     # 손실 그래프
     plt.subplot(1, 2, 1)
     plt.plot(history.history['loss'], label='Train Loss')
     plt.plot(history.history['val_loss'], label='Validation Loss')
-    plt.title('Loss')
+    plt.title('Loss' if title is None else f'Loss - {title}')
     plt.xlabel('Epochs')
     plt.ylabel('Loss')
     plt.legend()
@@ -16,9 +16,10 @@ def plot_history(history):
     plt.subplot(1, 2, 2)
     plt.plot(history.history['mae'], label='Train MAE')
     plt.plot(history.history['val_mae'], label='Validation MAE')
-    plt.title('Mean Absolute Error')
+    plt.title('Mean Absolute Error' if title is None else f'MAE - {title}')
     plt.xlabel('Epochs')
     plt.ylabel('MAE')
     plt.legend()
 
+    plt.tight_layout()
     plt.show()
