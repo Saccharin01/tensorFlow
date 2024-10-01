5776d6c3bfc85ad7e7c4f6d8a1ae4c3a489d38ba
조우식 <chousik01@gmail.com>
2024-10-01 11:11:47 +0900

    new | FastAPI_backend.py

--- example/FastAPI_backend.py
+++ example/FastAPI_backend.py
@@ -30,4 +30,4 @@ async def predict(file: UploadFile = File(...)):
         raise HTTPException(status_code=500, detail=f"Prediction failed: {str(e)}")
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8001)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
