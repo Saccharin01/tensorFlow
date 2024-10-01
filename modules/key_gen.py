1518f4082143ecd37a34365bcf83a3bca40c505e
조우식 <chousik01@gmail.com>
2024-10-01 11:41:52 +0900

    feat | key_gen.py
    지도 학습 과정에서 json 파일의 key 값을 읽어들이는 핼퍼 모듈입니다.
    프로토타입인 keygen, keygen2와 integration 하였습니다.

---
+++ modules/key_gen.py
@@ -0,0 +1,35 @@
+import json
+from typing import List, Optional
+
+
+def key_gen(json_path : str, remove_key : Optional[str]=None) -> List[str]:
+    """
+    JSON 파일에서 특정 키를 제거한 나머지 키를 추출하는 함수입니다.
+    remove_key의 값이 전달되지 않으면 json 데이터의 모든 key 값을 반환합니다.
+
+    * Args:
+        json_path (str): JSON 파일의 경로
+        remove_key (str): 제거할 키의 이름
+
+    * Returns:
+        List[str]: 제거된 키를 제외한 나머지 키 목록
+    """
+    try:
+        with open(json_path, "r") as f:
+            json_data = json.load(f)
+            
+        class_list = [key for key in json_data[0] if key != remove_key]
+        
+        print(class_list)
+        
+        return class_list
+    
+    except FileNotFoundError:
+        
+        print(f"입력하신 파일을 찾을 수 없습니다 : {json_path}")
+        return []
+
+
+
+# json_path = 'athletes_data.json'
+# key_gen(json_path)
