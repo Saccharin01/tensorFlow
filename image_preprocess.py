fa6350b08fa3ce2fca1fc1f3a7499beab2455faf
조우식 <chousik01@gmail.com>
2024-10-01 12:29:36 +0900

    new | image_preprocess.py
    이미지 파일을 전처리 하는 함수입니다.

---
+++ image_preprocess.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+from typing import List, Tuple
+
+def preprocess_image(img_path: str, target_size : Tuple[int,int]=(128, 256)) -> tf.Tensor:
+    """
+    주어진 경로의 이미지를 읽고, 크기 조정 및 정규화를 수행하는 함수입니다.
+
+    Args:
+        img_path (str): 이미지 파일 경로
+        target_size (tuple, optional): 이미지 크기 조정 (width, height), 기본값 (128, 256)
+
+    Returns:
+        tf.Tensor: 전처리된 이미지 텐서
+    """
+    
+    img_process = tf.keras.preprocessing.image
+    img = img_process.load_img(img_path, target_size=target_size)
+    img_array = img_process.img_to_array(img) / 255.0  # 정규화
+    
+    return img_array
